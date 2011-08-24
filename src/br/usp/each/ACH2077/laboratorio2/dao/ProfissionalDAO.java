978f2c2c3921566449d1da557ac86acafb40663b
Felipe Pierin <felipe.pierin@gmail.com>
2011-08-24 17:05:50 -0300

    estabelecimento de connection por jdbc ao servdior postgre

--- src/br/usp/each/ACH2077/laboratorio2/dao/ProfissionalDAO.java
+++ src/br/usp/each/ACH2077/laboratorio2/dao/ProfissionalDAO.java
@@ -1,5 +1,15 @@
 package br.usp.each.ach2077.laboratorio2.dao;
 
+import java.sql.Connection;
+
 public class ProfissionalDAO {
+	
+	private Connection connection;
+
+	public ProfissionalDAO(final Connection connection) {
+		this.connection = connection;
+	}		
+	
+	
 
 }
